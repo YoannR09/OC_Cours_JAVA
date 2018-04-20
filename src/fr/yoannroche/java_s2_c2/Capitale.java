42fbe869e548cc636bb5a58e8e97e9231a12b240
YoannR09 <el-rambo-poto@hotmai.fr>
2018-04-20 15:50:03 +0200

    Fin du chapitre 2 section 2

---
+++ src/fr/yoannroche/java_s2_c2/Capitale.java
@@ -0,0 +1,40 @@
+package fr.yoannroche.java_s2_c2;
+
+ class Capitale extends Ville {
+	 
+	 private String monument;
+	 
+	//Constructeur par défaut
+	  public Capitale(){
+	    //Ce mot clé appelle le constructeur de la classe mère  
+	    super();
+	    monument = "aucun";
+	  }
+	  
+	//Constructeur d'initialisation de capitale
+	  public Capitale(String nom, int hab, String pays, String monument){
+		super(nom, hab, pays);
+	    this.monument = monument;
+	    
+	  }
+	  
+	  public String decrisToi(){
+		    String str =  super.decrisToi() + "\n \t ==>>" + this.monument+ " en est un monument";
+		    System.out.println("Invocation de super.decrisToi()");
+		    
+		    return str;
+		   }
+	  /**
+	    * @return le nom du monument
+	  */
+	  public String getMonument() {
+	    return monument;
+	  } 
+
+	  //Définit le nom du monument
+	  public void setMonument(String monument) {
+	    this.monument = monument;
+	  }   
+	  
+
+}
---
+++ src/fr/yoannroche/java_s2_c2/Ville.java
@@ -0,0 +1,61 @@
+package fr.yoannroche.java_s2_c2;
+
+
+public class Ville {
+
+	public static int nbreInstances = 0;
+
+	protected String nomVille;
+	protected int nbreHabitants;
+	protected String nomPays;
+	protected char categorie;
+	protected static int nbreInstancesBis = 0;
+	protected String monument;
+
+	public Ville(){          
+		nomVille = "Inconnu";
+		nomPays = "Inconnu";
+		nbreHabitants = 0;
+		monument = "Inconnu";
+		Capitale cap = new Capitale("Paris", 654987, "France", "la tour Eiffel");
+	    System.out.println("\n"+cap.decrisToi());
+	} 
+
+	//Retourne le nom de la ville
+	public String getNom()  {  
+		return nomVille;
+	}
+
+	public char getCategorie()
+	{
+		return categorie;
+	} 
+	//Retourne le nom du pays
+	public String getNomPays()
+	{
+		return nomPays;
+	}
+
+	// Retourne le nombre d'habitants
+	public int getNombreHabitants()
+	{
+		return nbreHabitants;
+	} 
+	public static int getNombreInstancesBis()
+	{
+		return nbreInstancesBis;
+	}  
+	public void setNom(String pNom)
+	{
+		nomVille = pNom;
+	}
+	
+	public String decrisToi(){
+		return "\t"+this.nomVille+" est une ville de "+this.nomPays+ ", elle comporte : "+this.nbreHabitants+" habitant(s) => elle est donc de catégorie : "+this.categorie;
+	}
+
+	public static void main(String[] args) {
+	}
+	
+}
+
