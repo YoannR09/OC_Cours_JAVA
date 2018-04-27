afe23f76ef8b734807978bbb80693b7d189d2159
YoannR09 <el-rambo-poto@hotmai.fr>
2018-04-27 18:29:06 +0200

    Modif de la Main

--- src/fr/yoannroche/java_s2_c11_lesflux/Main.java
+++ src/fr/yoannroche/java_s2_c11_lesflux/Main.java
@@ -1,36 +1,58 @@
 package fr.yoannroche.java_s2_c11_lesflux;
 
 //Packages à importer afin d'utiliser l'objet File
-import java.io.CharArrayReader;
-import java.io.CharArrayWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Main {
 	public static void main(String[] args) {
-		CharArrayWriter caw = new CharArrayWriter();
-		CharArrayReader car;
-
+		//Nous déclarons nos objets en dehors du bloc try/catch
+		ObjectInputStream ois;
+		ObjectOutputStream oos;
 		try {
-			caw.write("Coucou les Zéros");
-			//Appel à la méthode toString de notre objet de manière tacite
-			System.out.println(caw);
+			oos = new ObjectOutputStream(
+					new BufferedOutputStream(
+							new FileOutputStream(
+									new File("game.txt"))));
 
-			//caw.close() n'a aucun effet sur le flux
-			//Seul caw.reset() peut tout effacer
-			caw.close();
+			//Nous allons écrire chaque objet Game dans le fichier
+			oos.writeObject(new Game("Assassin Creed", "Aventure", 45.69));
+			oos.writeObject(new Game("Tomb Raider", "Plateforme", 23.45));
+			oos.writeObject(new Game("Tetris", "Stratégie", 2.50));
+			//Ne pas oublier de fermer le flux !
+			oos.close();
 
-			//On passe un tableau de caractères à l'objet qui va lire le tampon
-			car = new CharArrayReader(caw.toCharArray());
-			int i;
-			//On remet tous les caractères lus dans un String
-			String str = "";
-			while(( i = car.read()) != -1)
-				str += (char) i;
+			//On récupère maintenant les données !
+			ois = new ObjectInputStream(
+					new BufferedInputStream(
+							new FileInputStream(
+									new File("game.txt"))));
 
-			System.out.println(str);
+			try {
+				System.out.println("Affichage des jeux :");
+				System.out.println("*************************\n");
+				System.out.println(((Game)ois.readObject()).toString());
+				System.out.println(((Game)ois.readObject()).toString());
+				System.out.println(((Game)ois.readObject()).toString());
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 
+			ois.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}     	
 	}
 }
